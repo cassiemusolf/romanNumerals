7d3cae5df42ee532496539a1376e0c29cb04ca05
Cassie Musolf and Kaz Matthews <kaz@>
2017-01-19 13:58:11 -0800

    add if else statements to replace M, C, X and I strings for 4 and 9, with other roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -12,36 +12,72 @@ var digitTranslate = function(number) {
 
   var singleNumberArray = number.split("");
   var firstDigit = parseInt(singleNumberArray[0]);
-  var emptyArray = [];
+  var emptyArrayM = [];
+  var emptyArrayC = [];
+  var emptyArrayX = [];
+  var emptyArrayI = [];
+
 
   if (singleNumberArray.length === 4) {
     for (index=0; index < firstDigit; index++) {
-      emptyArray.push("M");
+      emptyArrayM.push("M");
     }
     singleNumberArray.splice(0, 1);
     firstDigit = parseInt(singleNumberArray[0]);
   }
   if (singleNumberArray.length === 3) {
     for (index=0; index < firstDigit; index++) {
-      emptyArray.push("C");
+      emptyArrayC.push("C");
     }
     singleNumberArray.splice(0,1);
     firstDigit = parseInt(singleNumberArray[0]);
   }
   if (singleNumberArray.length === 2) {
     for (index=0; index < firstDigit; index++) {
-      emptyArray.push("X");
+      emptyArrayX.push("X");
     }
     singleNumberArray.splice(0,1);
     firstDigit = parseInt(singleNumberArray[0]);
   }
   if (singleNumberArray.length === 1) {
     for (index=0; index < firstDigit; index++) {
-      emptyArray.push("I");
+      emptyArrayI.push("I");
     }
   }
-    return emptyArray;
+
+  var cLength = emptyArrayC.length;
+
+  if (cLength >= 5 && cLength <= 8) {
+    emptyArrayC.splice(0, 5, "D");
+  } else if (cLength === 4) {
+    emptyArrayC.splice(0, 4, "CD");
+  } else if (cLength === 9) {
+    emptyArrayC.splice(0, 9, "CM");
+  }
+
+  var xLength = emptyArrayX.length;
+
+  if (xLength >= 5 && xLength <= 8) {
+    emptyArrayX.splice(0, 5, "L");
+  } else if (xLength === 4) {
+    emptyArrayX.splice(0, 4, "XL");
+  } else if (xLength === 9) {
+    emptyArrayX.splice(0, 9, "XC");
+  }
+
+  var iLength = emptyArrayI.length;
+
+  if (iLength >= 5 && iLength <= 8) {
+    emptyArrayI.splice(0, 5, "V");
+  } else if (iLength === 4) {
+    emptyArrayI.splice(0, 4, "IV");
+  } else if (iLength === 9) {
+    emptyArrayI.splice(0, 9, "IX");
+  }
+  return emptyArrayI;
 }
+
+
 
 
 $ (document).ready(function() {
