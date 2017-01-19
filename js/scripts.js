479f8041a839e6b01e64f6a3e31ddfe60978f9f4
Cassie Musolf and Kaz Matthews <cassie@>
2017-01-19 08:57:22 -0800

    add array of roman numerals and assign numbers to each roman numeral and create function that returns true if user input is less than four thousand.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
+var I = 1;
+var V = 5;
+var X = 10;
+var L = 50;
+var C = 100;
+var D = 500;
+var M = 1000;
+var romanNumerals = [M, D, C, L, X, V, I];
+
+var checkNumber = function(number) {
+  var goodNumber = true;
+  if (number >= 4000){
+    goodNumber = false;
+  }
+  return goodNumber;
+}
+
+
+
 $ (document).ready(function() {
   $("form#romanNumerals").submit(function(event){
     event.preventDefault();
 
     var userInput = parseInt($("input#number1").val());
-    $("#results").text(userInput);
+    var result = checkNumber(userInput);
+    $("#results").text(result);
   });
 });
